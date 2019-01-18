f447d2dd3da059974730f45169816f137697d71d
lore0223 <loremma23@gmail.com>
2019-01-18 10:00:23 -0600

    subiendo texto alienado y ultimos cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,45 +1,34 @@
 window.cipher = {
-  encode:(desplazamiento,cifrar) =>{
-  let mayusculas=cifrar.toUpperCase();//convirtiendo a mayusculas el mensaje ingresado.
-//aqui vamos a iterar el mensaje y aplicarle la formula,
- //y convertir el resultado de la formula de nuevo a string
- // y obtener así el nuevo cifrado
-let nuevoCifrado= '';
-  for(let i=0;i<mayusculas.length; i++){
-    const letraAscii=mayusculas.charCodeAt(i);
-    const formula=(letraAscii-65+parseInt(desplazamiento))%26+65;
-    const resultado= String.fromCharCode(formula);
-    nuevoCifrado += resultado;
-  }
-  return nuevoCifrado;
-  },
+  encode: (desplazamiento, cifrar) => {
+    let mayusculas = cifrar.toUpperCase(); //convirtiendo a mayusculas el mensaje ingresado.
+    let nuevoCifrado = '';
     
-  decode:(desplazamiento2,descifrar) => {
-    let mayusculas=descifrar.toUpperCase();
-
-    let mnsjDescifrado='';
-
-    for(let i= 0;i<mayusculas.length; i++){
-      const letraAscii=mayusculas.charCodeAt(i);
-      const formulaDescifrar=(letraAscii+65-parseInt(desplazamiento2))%26+65;
-      const resultado2=String.fromCharCode(formulaDescifrar);
-       mnsjDescifrado += resultado2;
-      
+    //aqui vamos a iterar el mensaje,convertir la letra a codigo ascii y aplicarle la formula,
+    // convertir el resultado de la formula de nuevo a string
+    // y obtener así el nuevo cifrado
+    
+    for (let i = 0; i < mayusculas.length; i++) {
+      const letraAscii = mayusculas.charCodeAt(i);
+      const formula = (letraAscii - 65 + parseInt(desplazamiento)) % 26 + 65;
+      const resultado = String.fromCharCode(formula);
+      nuevoCifrado += resultado;
     }
-return mnsjDescifrado
-  }
-  // obtiene el codigo ascii y convierte a mayusculas
-/*function mnsj(){
-  let mensajeObtenido= mensaje.value;
-let mayuscula= mensajeObtenido.toUpperCase();
-let mayus=mayuscula.charCodeAt();
+    return nuevoCifrado;
+  },
+   //función para decodificar el mensaje, basicamente se realizan los mismos pasos
+   //y en la formula se invierte la resta por una suma y la suma por una resta.
+  decode: (desplazamiento2, descifrar) => {
+    let mayusculas = descifrar.toUpperCase();
+    let mnsjDescifrado = '';
 
-for(let i=0; i<mayuscula.length; i++){
-  let letraAscii=mayuscula.charCodeAt(i);
-  let formula=(letraAscii-65+ offset)%26+65;
-  let resultado= String.fromCharCode(formula);
-  nuevoCifrado += resultado;
-}
-alert (nuevoCifrado);*/
+    for (let i = 0; i < mayusculas.length; i++) {
+      const letraAscii = mayusculas.charCodeAt(i);
+      const formulaDescifrar = (letraAscii + 65 - parseInt(desplazamiento2)) % 26 + 65;
+      const resultado2 = String.fromCharCode(formulaDescifrar);
+      mnsjDescifrado += resultado2;
+
+    }
+    return mnsjDescifrado
+  }
 
 };
--- src/index.js
+++ src/index.js
@@ -1,68 +1,52 @@
 //constantes globales
-const box1=document.getElementById('box1');//caja principal
-const box2=document.getElementById('box2');//caja muestra el cifrado
-const box3=document.getElementById('box3');//caja que muestra el descifrado
-const encriptar=document.getElementById('encriptar');//boton para cifrar
-const desencriptar=document.getElementById('desencriptar');//boton para descifrar
-const volver1= document.getElementById('volver1');//regreso a pagina principal
-const volver2=document.getElementById('volver2');//regreso a pagina principal
-const mnsjEncriptado=document.getElementById('mnsj-encriptado');//mensaje cifrado
-const mnsjDesencriptado=document.getElementById('mnsj-desencriptado');//mensaje ya descifrado
+const box1 = document.getElementById('box1'); //caja principal
+const box2 = document.getElementById('box2'); //caja muestra el cifrado
+const box3 = document.getElementById('box3'); //caja que muestra el descifrado
+const encriptar = document.getElementById('encriptar'); //boton para cifrar
+const desencriptar = document.getElementById('desencriptar'); //boton para descifrar
+const volver1 = document.getElementById('volver1'); //regreso a pagina principal
+const volver2 = document.getElementById('volver2'); //regreso a pagina principal
+const mnsjEncriptado = document.getElementById('mnsj-encriptado'); //mensaje cifrado
+const mnsjDesencriptado = document.getElementById('mnsj-desencriptado'); //mensaje ya descifrado
 
-//el boton encriptar tiene la funcion de ocultar las cajas,obtiene el valor del input
+//El boton encriptar tiene la funcion de ocultar las cajas(div) y obtiene el valor de textarea e input
 //ingresado por el usuario.Al offset se le aplico el metodo parseInt por que el 
 //input number devuelve un string.
-//por medio de window.cipher.encode invocamos la función y pasamos argumentos
-//con el innerHtml pintamos en la pantalla el resultado de la función
-encriptar.addEventListener('click',()=>{
-   box1.classList.add('hideElement');
-   box2.classList.remove('hideElement');
-   const mensajeCifrado= document.getElementById('mensaje-usuario').value; 
-   const offset=parseInt(document.getElementById('offset').value);
+//Por medio de window.cipher.encode invocamos la función y pasamos argumentos obtenidos.
+//Con el innerHtml pintamos en la pantalla el resultado de la función.
 
-  const mensajito=window.cipher.encode(offset,mensajeCifrado);
-  mnsjEncriptado.innerHTML=mensajito;
+encriptar.addEventListener('click', () => {
+  box1.classList.add('hideElement');
+  box2.classList.remove('hideElement');
+  const mensajeCifrado = document.getElementById('mensaje-usuario').value;
+  const offset = parseInt(document.getElementById('offset').value);
+
+  const mensajito = window.cipher.encode(offset, mensajeCifrado);
+  mnsjEncriptado.innerHTML = mensajito;
 })
 
-desencriptar.addEventListener('click',()=>{
-   box1.classList.add('hideElement');
-   box3.classList.remove('hideElement');
-   const mensajeDescifrado=document.getElementById('mensaje-usuario').value;
-   const offset2=parseInt(document.getElementById('offset').value);
+desencriptar.addEventListener('click', () => {
+  box1.classList.add('hideElement');
+  box3.classList.remove('hideElement');
+  const mensajeDescifrado = document.getElementById('mensaje-usuario').value;
+  const offset2 = parseInt(document.getElementById('offset').value);
 
-   const mensajitoRevelado=window.cipher.decode(offset2,mensajeDescifrado);
-   mnsjDesencriptado.innerHTML=mensajitoRevelado;
+  const mensajitoRevelado = window.cipher.decode(offset2, mensajeDescifrado);
+  mnsjDesencriptado.innerHTML = mensajitoRevelado;
 
 
 })
+//los botones volver1 y volver 2 tienen la función de refrescar la pagina 
+//y nos regresan a la caja principal.
 
-volver1.addEventListener('click',()=>{
-   location.reload();
-   box2.classList.add('hideElement');
-   box1.classList.remove('hideElement');
-})
+volver1.addEventListener('click', () => {
+  location.reload();
+  box2.classList.add('hideElement');
+  box1.classList.remove('hideElement');
+});
 
-volver2.addEventListener('click',()=>{
-   location.reload()
-    box3.classList.add('hideElement');
-    box1.classList.remove('hideElement');
- })
- 
- 
-    
-   
-
-
-
-
-
-
-// function prueba(num1,num2){
-//     return num1+num2
-// }
-
-// const prueba = function(num1,num2){
-//     return num1+num2
-// }
-
-// const prueba = num1 =>  num1+num2
+volver2.addEventListener('click', () => {
+  location.reload();
+  box3.classList.add('hideElement');
+  box1.classList.remove('hideElement');
+});
